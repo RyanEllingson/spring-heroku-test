4106f1984006a1547baad803a6689605cf4550b7
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2021-02-15 20:54:34 -0600

    start spring boot application

---
+++ src/main/java/com/ryan/herokutest/SpringHerokuTestApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.herokutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHerokuTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHerokuTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryan/herokutest/SpringHerokuTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.herokutest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHerokuTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
